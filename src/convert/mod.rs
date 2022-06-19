3fd3134379adc74b4d20d0dc504227937c1dbc8c
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-06-18 22:32:45 -0500

    Update `convert/mod.rs` to use `convert/parser.rs`

--- src/convert/mod.rs
+++ src/convert/mod.rs
@@ -18,16 +18,18 @@
 // https://github.com/rust-lang/rust-clippy/pull/8271
 #![allow(clippy::ptr_arg)]
 
+mod parser;
+
 use clap::ArgEnum;
 use indexmap::IndexMap;
 use std::string::String;
 
 use crate::cli::Shell;
 use crate::config_file::{EnvVariableOption, EnvVariableValue, EnvironmentVariables};
+use crate::convert::parser::ValuePartKind;
 
+/// Converts the hash table of `vars` into a script for the given `shell`.
 pub(crate) fn to_shell_source(vars: &EnvironmentVariables, shell: &Shell) -> String {
-    //! Converts the hash table of `vars` into a script for the given `shell`.
-
     let mut output = String::new();
     for (name, variable_option) in vars {
         // Check whether the current item is a single environment var or a table of specific shells.
@@ -51,11 +53,13 @@ pub(crate) fn to_shell_source(vars: &EnvironmentVariables, shell: &Shell) -> Str
                 continue;
             }
 
-            EnvVariableValue::String(string) => (expand_value(string.as_str()), false),
-            EnvVariableValue::Set(true) => ("1".to_string(), false),
+            EnvVariableValue::String(string) => (expand_value(string.as_str(), shell), false),
+            EnvVariableValue::Set(true) => (String::from("1"), false),
             EnvVariableValue::Array(array) => {
-                let v_expanded: Vec<String> =
-                    array.iter().map(|value| expand_value(value)).collect();
+                let v_expanded: Vec<String> = array
+                    .iter()
+                    .map(|value| expand_value(value, shell))
+                    .collect();
                 (v_expanded.join(":"), true)
             }
         };
@@ -89,7 +93,7 @@ mod add_script_line {
         // Select the correct form for the chosen shell.
         *output += &match shell {
             Shell::Bash | Shell::Zsh => {
-                format!("export {}=\"{}\";\n", name, value)
+                format!("export {}=", name)
             }
             Shell::Fish => {
                 // Add `--path` to the variable if the variable is represented as a list.
@@ -97,9 +101,12 @@ mod add_script_line {
                     true => " --path",
                     false => "",
                 };
-                format!("set -gx{path} {} \"{}\";\n", name, value, path = path)
+                format!("set -gx{path} {} ", name, path = path)
             }
         };
+
+        *output += value;
+        *output += ";\n";
     }
 
     pub fn unset_variable(output: &mut String, shell: &Shell, name: &str) {
@@ -109,47 +116,92 @@ mod add_script_line {
         // Select the correct form for the chosen shell.
         *output += &match shell {
             Shell::Bash | Shell::Zsh => {
-                format!("unset {};\n", name)
+                format!("unset {}", name)
             }
             Shell::Fish => {
-                format!("set -ge {};\n", name)
+                format!("set -ge {}", name)
             }
         };
+
+        *output += ";\n";
     }
 }
 
+/// Given a `shell` and a `map` of all specific shell options, get the correct shell `EnvVariableValue`.
+/// Used by `to_shell_source` to filter the right `EnvVariableOption::Specific` for the current shell.
 fn value_for_specific<'a>(
     shell: &Shell,
     map: &'a IndexMap<String, EnvVariableValue>,
 ) -> Option<&'a EnvVariableValue> {
-    //! Given a `shell` and a `map` of all specific shell options, get the correct shell `EnvVariableValue`.
-    //! Used by `to_shell_source` to filter the right `EnvVariableOption::Specific` for the current shell.
     let shell_name = shell.to_possible_value()?.get_name();
     map.get(shell_name).or_else(|| map.get("_"))
 }
 
-fn expand_value(value: &str) -> String {
-    //! Expand the literal representation of a string in the toml
-    //! to a value with escape characters escaped and shell variables expanded.
+/// Expand the literal representation of a string in the toml to a value that can be parsed by the
+/// given shell.
+fn expand_value(value: &str, shell: &Shell) -> String {
+    use Shell::*;
+    use ValuePartKind::*;
 
-    // Replace TOML escape codes with the literal representation so that they are correctly used.
-    // We need to do this for every code listed here: https://toml.io/en/v1.0.0#string
-    let value = value
-        .replace('\\', r"\\") // Backslash - Must be first!
-        .replace('\x08', r"\b") // Backspace
-        .replace('\t', r"\t") // Tab
-        .replace('\n', r"\n") // Newline
-        .replace('\x0C', r"\f") // Form Feed
-        .replace('\r', r"\r") // Carriage Return
-        .replace('\"', "\\\""); // Double Quote
+    let value_parts = parser::parse_value(value);
 
-    // Expand tildes
-    shellexpand::tilde(&value).to_string()
+    let mut expanded_value = String::new();
+
+    // Handle each part for the specified shell, and concatenate each part together.
+    for part in value_parts {
+        let parser::ValuePart { value, kind } = part;
+
+        expanded_value += &match (kind, shell) {
+            (Literal, _) => single_quote(value, shell),
+
+            (ShellVariable, Bash | Zsh) => format!("${{{}}}", value),
+            (ShellVariable, Fish) => format!("{{${}}}", value),
+
+            (ShellCommand, Bash | Zsh) => format!("$(eval {})", single_quote(value, shell)),
+            (ShellCommand, Fish) => format!("(eval {})", single_quote(value, shell)),
+
+            (Home, Bash | Zsh) => {
+                format!("$(eval echo \"~{}\")", value)
+            }
+            (Home, Fish) => format!("(eval echo \"~{}\")", value),
+        }
+    }
+    expanded_value
+}
+
+// Surround a string in single quotes in a way that is best suited for a specific shell.
+// Specifically, Fish shell has a simpler way of escaping single quotes in a single quoted string,
+// while Bash and Zsh have to do it another way.
+fn single_quote(string: String, shell: &Shell) -> String {
+    use Shell::*;
+    match shell {
+        Bash | Zsh => string
+            // Bash and Zsh can't escape any single quotes within a single-quoted string,
+            // so whenever we encounter one we need to get the current string, begin a
+            // double-quoted string containing the single quote, and then start a new
+            // single-quoted string.
+            .split('\'')
+            .map(|v| {
+                // If there are no characters between single quotes, or if a single quote begins or
+                // ends a string, don't add an additional two single quotes (`''`) to the output.
+                if v.is_empty() {
+                    String::new()
+                } else {
+                    // There will be something between the quotes, so add the quotes!
+                    format!("'{}'", v)
+                }
+            })
+            .collect::<Vec<String>>()
+            .join(r#""'""#),
+        Fish => format!("'{}'", string.replace('\\', "\\\\").replace('\'', "\\'")),
+    }
 }
 
 #[cfg(test)]
-mod test {
+mod test_conversion {
     use super::*;
+    use pretty_assertions::{assert_eq, assert_str_eq};
+
     use crate::config_file::*;
 
     use EnvVariableOption::*;
@@ -157,7 +209,6 @@ mod test {
     use indexmap::indexmap;
     use indoc::indoc;
     use maplit::hashmap;
-    use pretty_assertions::{assert_eq, assert_str_eq};
     use std::collections::HashMap;
 
     // Used to test whether a file in toml format can be converted ito a correct `IndexMap`,
@@ -203,9 +254,9 @@ mod test {
             },
             // language=sh
             hashmap! {
-                Shell::Bash => r#"export FOO="Bar";"#,
-                Shell::Zsh => r#"export FOO="Bar";"#,
-                Shell::Fish => r#"set -gx FOO "Bar";"#,
+                Shell::Bash => r#"export FOO='Bar';"#,
+                Shell::Zsh => r#"export FOO='Bar';"#,
+                Shell::Fish => r#"set -gx FOO 'Bar';"#,
             },
         )
     }
@@ -228,9 +279,9 @@ mod test {
             },
             // language=sh
             hashmap! {
-                Shell::Bash => r#"export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";"#,
-                Shell::Zsh => r#"export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";"#,
-                Shell::Fish => r#"set -gx --path PATH "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";"#,
+                Shell::Bash => r#"export PATH='/usr/local/bin':'/usr/bin':'/bin':'/usr/sbin':'/sbin';"#,
+                Shell::Zsh => r#"export PATH='/usr/local/bin':'/usr/bin':'/bin':'/usr/sbin':'/sbin';"#,
+                Shell::Fish => r#"set -gx --path PATH '/usr/local/bin':'/usr/bin':'/bin':'/usr/sbin':'/sbin';"#,
             },
         )
     }
@@ -247,9 +298,9 @@ mod test {
             },
             // language=sh
             hashmap! {
-                Shell::Bash => r#"export HOMEBREW_NO_ANALYTICS="1";"#,
-                Shell::Zsh => r#"export HOMEBREW_NO_ANALYTICS="1";"#,
-                Shell::Fish => r#"set -gx HOMEBREW_NO_ANALYTICS "1";"#,
+                Shell::Bash => r#"export HOMEBREW_NO_ANALYTICS=1;"#,
+                Shell::Zsh => r#"export HOMEBREW_NO_ANALYTICS=1;"#,
+                Shell::Fish => r#"set -gx HOMEBREW_NO_ANALYTICS 1;"#,
             },
         )
     }
@@ -287,7 +338,7 @@ mod test {
             },
             // language=sh
             hashmap! {
-                Shell::Bash => r#"export ONLY_FOR_BASH="Do people read test cases?";"#,
+                Shell::Bash => r#"export ONLY_FOR_BASH='Do people read test cases?';"#,
                 Shell::Zsh => "",
                 Shell::Fish => "",
             },
@@ -310,9 +361,9 @@ mod test {
             },
             // language=sh
             hashmap! {
-                Shell::Bash => r#"export SOME_VARIABLE="[ACCESS DENIED]";"#,
-                Shell::Zsh => r#"export SOME_VARIABLE="[ACCESS DENIED]";"#,
-                Shell::Fish => r#"set -gx SOME_VARIABLE "you're pretty";"#,
+                Shell::Bash => r#"export SOME_VARIABLE='[ACCESS DENIED]';"#,
+                Shell::Zsh => r#"export SOME_VARIABLE='[ACCESS DENIED]';"#,
+                Shell::Fish => r#"set -gx SOME_VARIABLE 'you\'re pretty';"#,
             },
         )
     }
@@ -340,12 +391,139 @@ mod test {
             },
             // language=sh
             hashmap! {
-                Shell::Bash => r#"export SOME_VARIABLE="[ACCESS DENIED]";"#,
+                Shell::Bash => r#"export SOME_VARIABLE='[ACCESS DENIED]';"#,
                 Shell::Zsh => indoc! (r#"
-                    export SOME_VARIABLE="[ACCESS DENIED]";
-                    export ANOTHER_VARIABLE="Zzz";
+                    export SOME_VARIABLE='[ACCESS DENIED]';
+                    export ANOTHER_VARIABLE='Zzz';
                 "#),
-                Shell::Fish => r#"set -gx SOME_VARIABLE "you're pretty";"#,
+                Shell::Fish => r#"set -gx SOME_VARIABLE 'you\'re pretty';"#,
+            },
+        )
+    }
+
+    #[test]
+    fn test_shell_variables_inline() {
+        assert_convert(
+            // language=TOML
+            indoc! {r#"
+                WHERE_THE_HEART_IS = "$HOME"
+            "#},
+            indexmap! {
+                "WHERE_THE_HEART_IS".into() => General(EnvVariableValue::String("$HOME".into())),
+            },
+            // language=sh
+            hashmap! {
+                Shell::Bash => r#"export WHERE_THE_HEART_IS=${HOME};"#,
+                Shell::Zsh => r#"export WHERE_THE_HEART_IS=${HOME};"#,
+                Shell::Fish => r#"set -gx WHERE_THE_HEART_IS {$HOME};"#,
+            },
+        )
+    }
+
+    #[test]
+    fn test_shell_variables_block() {
+        assert_convert(
+            // language=TOML
+            indoc! {r#"
+                AN_EXAMPLE = "${HOME}less"
+            "#},
+            indexmap! {
+                "AN_EXAMPLE".into() => General(EnvVariableValue::String("${HOME}less".into())),
+            },
+            // language=sh
+            hashmap! {
+                Shell::Bash => r#"export AN_EXAMPLE=${HOME}'less';"#,
+                Shell::Zsh => r#"export AN_EXAMPLE=${HOME}'less';"#,
+                Shell::Fish => r#"set -gx AN_EXAMPLE {$HOME}'less';"#,
+            },
+        )
+    }
+
+    #[test]
+    fn test_shell_commands() {
+        assert_convert(
+            // language=TOML
+            indoc! {r#"
+                EDITOR = "$(which micro)"
+            "#},
+            indexmap! {
+                "EDITOR".into() => General(EnvVariableValue::String("$(which micro)".into())),
+            },
+            // language=sh
+            hashmap! {
+                Shell::Bash => r#"export EDITOR=$(eval 'which micro');"#,
+                Shell::Zsh => r#"export EDITOR=$(eval 'which micro');"#,
+                Shell::Fish => r#"set -gx EDITOR (eval 'which micro');"#,
+            },
+        )
+    }
+
+    #[test]
+    fn test_shell_home_tilde() {
+        assert_convert(
+            // language=TOML
+            indoc! {r#"
+                HOME = "~superatomic"
+            "#},
+            indexmap! {
+                "HOME".into() => General(EnvVariableValue::String("~superatomic".into())),
+            },
+            // language=sh
+            hashmap! {
+                Shell::Bash => r#"export HOME=$(eval echo "~superatomic");"#,
+                Shell::Zsh => r#"export HOME=$(eval echo "~superatomic");"#,
+                Shell::Fish => r#"set -gx HOME (eval echo "~superatomic");"#,
+            },
+        )
+    }
+
+    #[test]
+    fn test_convert_character_escapes() {
+        assert_convert(
+            // language=TOML
+            indoc! {r#"
+                MESSAGE = '''\$() is literal, and $(echo '\)') is escaped.'''
+                FAVORITE_CHARACTER = '\\'
+            "#},
+            indexmap! {
+                "MESSAGE".into() => General(EnvVariableValue::String(
+                    r"\$() is literal, and $(echo '\)') is escaped.".into()
+                )),
+                "FAVORITE_CHARACTER".into() => General(EnvVariableValue::String(r"\\".into())),
+            },
+            // language=sh
+            hashmap! {
+                Shell::Bash => indoc!(r#"
+                    export MESSAGE='$() is literal, and '$(eval 'echo '"'"')'"'")' is escaped.';
+                    export FAVORITE_CHARACTER='\';
+                "#),
+                Shell::Zsh => indoc!(r#"
+                    export MESSAGE='$() is literal, and '$(eval 'echo '"'"')'"'")' is escaped.';
+                    export FAVORITE_CHARACTER='\';
+                "#),
+                Shell::Fish => indoc!(r#"
+                    set -gx MESSAGE '$() is literal, and '(eval 'echo \')\'')' is escaped.';
+                    set -gx FAVORITE_CHARACTER '\\';
+                "#),
+            },
+        )
+    }
+
+    #[test]
+    fn test_convert_escaping_quotes() {
+        assert_convert(
+            // language=TOML
+            indoc! {r#"
+                MESSAGE = '''I 'love' books'''
+            "#},
+            indexmap! {
+                "MESSAGE".into() => General(EnvVariableValue::String(r"I 'love' books".into())),
+            },
+            // language=sh
+            hashmap! {
+                Shell::Bash => r#"export MESSAGE='I '"'"'love'"'"' books';"#,
+                Shell::Zsh => r#"export MESSAGE='I '"'"'love'"'"' books';"#,
+                Shell::Fish => r#"set -gx MESSAGE 'I \'love\' books';"#,
             },
         )
     }
@@ -358,8 +536,10 @@ mod test {
                 # A collection of random things for testing.
                 FOO = 'bar'
                 BAZ.zsh = 'zž'
-                BAZ.fish = ['gone', 'fishing']
-                BAZ._ = 'other'
+                BAZ.fish = ['gone', '$fishing']
+                BAZ._ = '~other'
+                TTY = '$(tty)'
+                THE_ECHO = '$(echo "\)")'
                 XSHE_IS_THE_BEST = true # look, idk.
                 # Return to poluting the home directory in bash
                 XDG_CONFIG_HOME.bash = false
@@ -368,9 +548,11 @@ mod test {
                 "FOO".into() => General(EnvVariableValue::String("bar".into())),
                 "BAZ".into() => Specific(indexmap! {
                     "zsh".into() => EnvVariableValue::String("zž".into()),
-                    "fish".into() => EnvVariableValue::Array(vec!["gone".into(), "fishing".into()]),
-                    "_".into() => EnvVariableValue::String("other".into()),
+                    "fish".into() => EnvVariableValue::Array(vec!["gone".into(), "$fishing".into()]),
+                    "_".into() => EnvVariableValue::String("~other".into()),
                 }),
+                "TTY".into() => General(EnvVariableValue::String("$(tty)".into())),
+                "THE_ECHO".into() => General(EnvVariableValue::String(r#"$(echo "\)")"#.into())),
                 "XSHE_IS_THE_BEST".into() => General(EnvVariableValue::Set(true)),
                 "XDG_CONFIG_HOME".into() => Specific(indexmap! {
                     "bash".into() => EnvVariableValue::Set(false),
@@ -379,20 +561,26 @@ mod test {
             // language=sh
             hashmap! {
                 Shell::Bash => indoc! (r#"
-                    export FOO="bar";
-                    export BAZ="other";
-                    export XSHE_IS_THE_BEST="1";
+                    export FOO='bar';
+                    export BAZ=$(eval echo "~other");
+                    export TTY=$(eval 'tty');
+                    export THE_ECHO=$(eval 'echo ")"');
+                    export XSHE_IS_THE_BEST=1;
                     unset XDG_CONFIG_HOME;
                 "#),
                 Shell::Zsh => indoc! (r#"
-                    export FOO="bar";
-                    export BAZ="zž";
-                    export XSHE_IS_THE_BEST="1";
+                    export FOO='bar';
+                    export BAZ='zž';
+                    export TTY=$(eval 'tty');
+                    export THE_ECHO=$(eval 'echo ")"');
+                    export XSHE_IS_THE_BEST=1;
                 "#),
                 Shell::Fish => indoc! (r#"
-                    set -gx FOO "bar";
-                    set -gx --path BAZ "gone:fishing";
-                    set -gx XSHE_IS_THE_BEST "1";
+                    set -gx FOO 'bar';
+                    set -gx --path BAZ 'gone':{$fishing};
+                    set -gx TTY (eval 'tty');
+                    set -gx THE_ECHO (eval 'echo ")"');
+                    set -gx XSHE_IS_THE_BEST 1;
                 "#),
             },
         )
